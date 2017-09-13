e3c402a9358c5a732492d06348425fe0142b2dbf
Jason Nelson <z1755548@students.niu.edu>
2017-09-12 20:09:12 -0700

    Initialize Repository

---
+++ Shorty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shorty
+//
+//  Created by Jason Nelson on 9/12/17.
+//  Copyright © 2017 Jason Nelson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Shorty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shorty
+//
+//  Created by Jason Nelson on 9/12/17.
+//  Copyright © 2017 Jason Nelson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
